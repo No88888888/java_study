ffaea8fc961ccf242be20bcacaf39a45ec2220a7
김진호 <wlsghsla94@naver.com>
2023-03-01 18:38:45 +0900

    0301

--- src/chap_03/_04_EscapeSequence.java
+++ src/chap_03/_04_EscapeSequence.java
@@ -7,6 +7,19 @@ public class _04_EscapeSequence {
         System.out.println("너무");
         System.out.println("재밌어요");
 
+        // 줄바꿈
         System.out.println("자바가\n 너무 \n재밌어요");
+
+        // 탭
+        System.out.println("해물파전\t\t9000원");
+        System.out.println("해물전\t\t8000원");
+        System.out.println("해물전\t\t7000원");
+
+        // \\: 역슬래시
+        System.out.println("C:\\Program files\\Java");
+
+        // 다비가 "냐옹"이라고 했어요
+        System.out.println("다비가 \"냐옹\"이라고 했어요");
+        System.out.println("다비가 '냐옹'이라고 했어요");
     }
 }
