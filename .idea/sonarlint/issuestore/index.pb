358c845da4fca72e7b07060b6d59b8233df24b0e
김진호 <wlsghsla94@naver.com>
2023-03-01 18:25:34 +0900

    0301

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+P
+ src/chap_01/_07_TypeCasting.java,e\0\e0072fbf1628c97f2db813bafd012947a3b70721
---
+++ src/chap_03/_04_EscapeSequence.java
@@ -0,0 +1,12 @@
+package chap_03;
+
+public class _04_EscapeSequence {
+    public static void main(String[] args) {
+        // 특수문자, 이스케이프 문자
+        System.out.println("자바가");
+        System.out.println("너무");
+        System.out.println("재밌어요");
+
+        System.out.println("자바가\n 너무 \n재밌어요");
+    }
+}
